e3737cda7028d2de1f31b4d531c2a3b4dbdac1ee
minkyum <moon.minkyu@gmail.com>
2018-01-31 15:24:42 -0500

    Edit for VIIRS and C6

--- 01_load_dates.R
+++ 01_load_dates.R
@@ -1,12 +1,54 @@
 # This code generate is for VIIRS validation.
-# 1. Load VIIRS and MCD12Q2 C6 files
+# 1. Load land cover type (MCD12Q1)
 
 rm(list = ls())
 # Loading required libraries 
 library(sp)
 library(raster)
+library(gdalUtils)
+library(rgeos)
+library(maptools)
+library(rgdal)
 library(RColorBrewer)
 
+### 1. Load land cover type (MCD12Q1) 
+# File path 
+mcd12q1_path <- '/projectnb/modislc/data/mcd12_out/lc_out/c5_hdf/c5.1_deliv/bug_fix_081214/mcd12q1/'
+# Info for specific time and tile 
+for(i in 1:3){
+  tile <- c('h12v04','h11v04','h08v05')
+  type = c('LC1','LC2','LC3','LC4','LC5')
+  years = seq(2001,2013)
+  LC <- vector('list',length(years))
+  lc <- vector('list',length(type))
+  for(j in 1:length(years)){
+    for(e in 1:length(type)){
+      mcd12q1_full_path <- paste(mcd12q1_path,'hdf_',years[j],'/',type[e],'/',
+                                 type[e],'.A',years[j],'001.',tile[i],'.hdf',sep='')
+      lc[[e]] = raster(mcd12q1_full_path)
+    }
+    LC[[j]] <- lc
+  }
+  if(i==1){
+    lct.1204 <- LC[[13]][[1]]  
+  }else if(i==2){
+    lct.1104 <- LC[[13]][[1]]  
+  }else{
+    lct.0805 <- LC[[13]][[1]]  
+  }
+}
+ext1204 <- raster('/projectnb/modislc/users/mkmoon/VIIRS/NLCD_crosswalked_IGBP/nlcd_igbp_500m/h12v04.bsq')
+ext1104 <- raster('/projectnb/modislc/users/mkmoon/VIIRS/NLCD_crosswalked_IGBP/nlcd_igbp_500m/h11v04.bsq')
+ext0805 <- raster('/projectnb/modislc/users/mkmoon/VIIRS/NLCD_crosswalked_IGBP/nlcd_igbp_500m/h08v05.bsq')
+
+lct1204 <- table(getValues(lct.1204))
+lct1104 <- table(getValues(lct.1104))
+lct0805 <- table(getValues(lct.0805))
+
+# File path
+viirs_path <- '/projectnb/modislc/users/mkmoon/VIIRS/tiles/h12v04/Dates_VIIRS/'
+modis_path6 <- '/projectnb/modislc/data/mcd12_out/phen_out/c6/'
+modis_path5 <- '/projectnb/modislc/data/mcd12_out/phen_out/c5_hdf1/'
 ##  all tiles
 path <- '/projectnb/modislc/users/dsm/eval_modis_lc_061917/MCD12I6/Dormancy/'
 search_str <- paste('*2001',sep='')
