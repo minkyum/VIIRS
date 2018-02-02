30cc7e2f9417bbcc1d70f5af4c4eadc603395754
minkyum <moon.minkyu@gmail.com>
2018-02-02 13:14:40 -0500

    Set file path and doy

--- 03_get_evi.R
+++ 03_get_evi.R
@@ -27,10 +27,10 @@ if(ss==1){
   files <- list.files(path=viirs_path,pattern=glob2rx(search_str),full.names=T,include.dirs=F)
   
   for(ii in 1:24){
-    evi_vv <- matrix(NA,length(files),240000)
-
+    evi_vv <- matrix(NA,366,240000)
     for(i in c(1:length(files))){
-      if(year==2012 | xx==2 | xx==3){       
+      doy <- as.numeric(substr(files[i],76,78))
+      if(year==2012 | xx==2 | xx==3){
         file <- h5file(name=files[i])
         dataset <- list.datasets(file)
         i1 <- readDataSet(file[dataset[[4]]])*0.0001
@@ -47,36 +47,25 @@ if(ss==1){
       temp <- 2.5*((i2-i1)/(i2 + (2.4*i1) +1)) # calculate EVI2 
       temp <- getValues(temp)
       
-      evi_vv[i,] <- temp[(ii*240000-239999):(ii*240000)]
+      evi_vv[doy,] <- temp[(ii*240000-239999):(ii*240000)]
     }
     
-    setwd('/projectnb/modislc/users/mkmoon/VIIRS/R_data/evi2_ts/')
+    setwd('/projectnb/modislc/users/mkmoon/VIIRS/R_data/evi2_ts/VIIRS/')
     d <- ii
     d_str <- sprintf('%03d',d)
     write.csv(evi_vv,file=paste('evi_vv_',tile,'_',year,'_',d_str,'.csv',sep=''))
   }
 }else{
-  if(xx==2 | xx==3){
-    # MCD43A4 -MODIS
-    modis_path <- '/projectnb/modislc/users/mkmoon/VIIRS/tiles/e4ftl01.cr.usgs.gov/MOTA/MCD43A4.006'
-    files <- list.files(path=modis_path,full.names=T,include.dirs=F,recursive=T)
-    
-    
-    for(ii in 1:24){
-      evi_mm <- matrix(NA,365,240000)
-      for(i in 1:365){
-        d <- seq(1,365)
-        d_str <- sprintf('%03d',d[i])
-        
-        dat <- as.Date(i,origin="2012-12-31")
-        year <- substr(dat,1,4)
-        mont <- substr(dat,6,7)
-        days <- substr(dat,9,10)
-        
-        path <- paste(modis_path,year,'.',mont,'.',days,'/',sep='')
-        s_str <- paste('*.A',year,d_str,'.',tile,'*.hdf',sep='')
-        full_path <- list.files(path=path,pattern=glob2rx(s_str),full.names=T,include.dirs=F)
-        sds <- get_subdatasets(full_path)
+  # MCD43A4 -MODIS
+  modis_path <- '/projectnb/modislc/users/mkmoon/VIIRS/tiles/e4ftl01.cr.usgs.gov/MOTA/MCD43A4.006'
+  search_str <- paste('*',year,'*',tile,'*',sep='')
+  files <- list.files(path=modis_path,pattern=glob2rx(search_str),full.names=T,include.dirs=F,recursive=T)
+  
+  for(ii in 1:24){
+    evi_mm <- matrix(NA,366,240000)
+      for(i in 1:length(files)){
+        doy <- as.numeric(substr(files[i],106,108))
+        sds <- get_subdatasets(files[i])
         
         b1 <- raster(sds[[8]])
         b2 <- raster(sds[[9]])
@@ -84,45 +73,15 @@ if(ss==1){
         temp <- 2.5*((b2-b1)/(b2 + (2.4*b1) +1)) # calculate EVI2 
         temp <- getValues(temp)
         
-        evi_mm[i,] <- temp[(ii*240000-239999):(ii*240000)]
+        evi_mm[doy,] <- temp[(ii*240000-239999):(ii*240000)]
       }
-      setwd('/projectnb/modislc/users/mkmoon/VIIRS/R_data/evi2_ts/')
-      d <- ii
-      d_str <- sprintf('%03d',d)
-      write.csv(evi_mm,file=paste('evi_mm_',tile,'_',year,'_',d_str,'.csv',sep=''))
-    }
-  }else{
-    # MCD43A4 - from Project cluster
-    modis_path <- '/projectnb/modislc/data/mcd12_in/c6/mcd43a4'
-    files <- list.files(path=modis_path,full.names=T,include.dirs=F,recursive=F)
-    
-    
-    for(ii in 1:24){
-      evi_mm <- matrix(NA,365,2400*100)
-      for(i in 1:365){
-        d <- seq(1,365)
-        d_str <- sprintf('%03d',d[i])
-        
-        path <- paste(modis_path,year,'/',d_str,'/',sep='')
-        s_str <- paste('*.A',year,d_str,'.',tile,'*.hdf',sep='')
-        full_path <- list.files(path=path,pattern=glob2rx(s_str),full.names=T,include.dirs=F)
-        sds <- get_subdatasets(full_path)
-        
-        b1 <- raster(sds[[8]])
-        b2 <- raster(sds[[9]])
-        
-        temp <- 2.5*((b2-b1)/(b2 + (2.4*b1) +1)) # calculate EVI2 
-        temp <- getValues(temp)
-        
-        evi_mm[i,] <- temp[(ii*240000-239999):(ii*240000)]
-      }
-      setwd('/projectnb/modislc/users/mkmoon/VIIRS/R_data/evi2_ts/')
-      d <- ii
-      d_str <- sprintf('%03d',d)
-      write.csv(evi_mm,file=paste('evi_mm_',tile,'_',year,'_',d_str,'.csv',sep=''))  
-    }
+    setwd('/projectnb/modislc/users/mkmoon/VIIRS/R_data/evi2_ts/MODIS/')
+    d <- ii
+    d_str <- sprintf('%03d',d)
+    write.csv(evi_mm,file=paste('evi_mm_',tile,'_',year,'_',d_str,'.csv',sep=''))
   }
 }
+
 
 
 
