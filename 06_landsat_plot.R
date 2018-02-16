6f74f11c806297981d17f8a4f1c35e51566f7194
minkyum <moon.minkyu@gmail.com>
2018-02-16 16:31:29 -0500

    load file name change

--- 06_landsat_plot.R
+++ 06_landsat_plot.R
@@ -11,9 +11,9 @@ qphe <- vector('list',12)
 ### SOS
 for(i in 1:12){
   if(i<=6){
-    load(paste('landsat_',scene[i],'_41.RData',sep=''))  
+    load(paste('landsat_c6_2013_sos_',scene[i],'.RData',sep=''))  
   }else{
-    load(paste('landsat_',scene[i-6],'_40.RData',sep=''))  
+    load(paste('landsat_c6_2012_sos_',scene[i-6],'.RData',sep=''))  
   }
   lphe[[i]] <- lanbymod
   mphe[[i]] <- mmphe
@@ -39,23 +39,23 @@ for(i in 1:11){
 
 ## 1:1 plot
 # all points
-for(i in 1:11){
-  if(i==1){
-    plot(mphe[[i]],pixmean[[i]],pch=i,
-         xlim=c(100,180),ylim=c(100,180),
-         main=scene[i])  
-    points(vphe[[i]],pixmean[[i]],col='red',pch=i)    
-    abline(0,1,lty=5)
-#     abline(lm(pixmean[[i]]~mphe[[i]]))
-  }else{
-    plot(mphe[[i]],pixmean[[i]],pch=i,
-         xlim=c(100,180),ylim=c(100,180),
-         main=scene[i])    
-    points(vphe[[i]],pixmean[[i]],col='red',pch=i)    
-    abline(0,1)
-#     abline(lm(pixmean[[i]]~mphe[[i]]))
-  } 
-}
+# for(i in 1:11){
+#   if(i==1){
+#     plot(mphe[[i]],pixmean[[i]],pch=i,
+#          xlim=c(100,180),ylim=c(100,180),
+#          main=scene[i])  
+#     points(vphe[[i]],pixmean[[i]],col='red',pch=i)    
+#     abline(0,1,lty=5)
+# #     abline(lm(pixmean[[i]]~mphe[[i]]))
+#   }else{
+#     plot(mphe[[i]],pixmean[[i]],pch=i,
+#          xlim=c(100,180),ylim=c(100,180),
+#          main=scene[i])    
+#     points(vphe[[i]],pixmean[[i]],col='red',pch=i)    
+#     abline(0,1)
+# #     abline(lm(pixmean[[i]]~mphe[[i]]))
+#   } 
+# }
 
 # Mean with error bars
 plot_1to1 <- function(xmean,ymean,xsd,ysd,xlabel,ylabel,start){
@@ -83,17 +83,17 @@ plot_1to1 <- function(xmean,ymean,xsd,ysd,xlabel,ylabel,start){
   b0c1 <- b0+round(reg$coefficients[3],1)
   b0c2 <- b0-round(reg$coefficients[3],1)
   rmse <- round(sqrt(mean((ymean-xmean)^2)),1)
-  text((start+33),(start+13),expression(paste(R^2,'=')),pos=4,cex=0.8)
-  text((start+38),(start+13),rseq,pos=4,cex=0.8)
-  text((start+45),(start+13),substitute(paste('(', italic(p),' < 0.01)',sep='')),pos=4,cex=0.8)
-  text((start+33),(start+9),paste('RMSE =',rmse),pos=4,cex=0.8)
-  text((start+33),(start+5),expression(paste(beta[1],'=')),pos=4,cex=0.8)
-  text((start+38),(start+5),paste(b1,' (',b1c1,',',b1c2,')',sep=''),pos=4,cex=0.8)
-  text((start+33),(start+1),expression(paste(beta[0],'=')),pos=4,cex=0.8)
-  text((start+38),(start+1),paste(b0,' (',b0c1,',',b0c2,')',sep=''),pos=4,cex=0.8)
+  text((start+33-1),(start+13),expression(paste(R^2,'=')),pos=4,cex=0.8)
+  text((start+38-1),(start+13),rseq,pos=4,cex=0.8)
+  text((start+45-1),(start+13),substitute(paste('(', italic(p),' < 0.01)',sep='')),pos=4,cex=0.8)
+  text((start+33-1),(start+9),paste('RMSE =',rmse),pos=4,cex=0.8)
+  text((start+33-1),(start+5),expression(paste(beta[1],'=')),pos=4,cex=0.8)
+  text((start+38-1),(start+5),paste(b1,' (',b1c1,',',b1c2,')',sep=''),pos=4,cex=0.8)
+  text((start+33-1),(start+1),expression(paste(beta[0],'=')),pos=4,cex=0.8)
+  text((start+38-1),(start+1),paste(b0,' (',b0c1,',',b0c2,')',sep=''),pos=4,cex=0.8)
 }
 
-par(mfrow=c(1,3),oma=c(1,1,1,1),mar=c(4,4,4,4),mgp=c(2.5,1,0))
+par(mfrow=c(2,3),oma=c(1,1,1,1),mar=c(4,4,4,4),mgp=c(2.5,1,0))
 plot_1to1(mesd[,1],mesd[,2],mesd[,4],mesd[,5],'MODIS SOS','VIIRS SOS',110)
 plot_1to1(mesd[,1],mesd[,3],mesd[,4],mesd[,6],'MODIS SOS','Landsat SOS',110)
 plot_1to1(mesd[,2],mesd[,3],mesd[,5],mesd[,6],'VIIRS SOS','Landsat SOS',110)
@@ -102,9 +102,9 @@ plot_1to1(mesd[,2],mesd[,3],mesd[,5],mesd[,6],'VIIRS SOS','Landsat SOS',110)
 ### EOS
 for(i in 1:12){
   if(i<=6){
-    load(paste('landsat_',scene[i],'_73.RData',sep=''))  
+    load(paste('landsat_c6_2013_eos_',scene[i],'.RData',sep=''))  
   }else{
-    load(paste('landsat_',scene[i-6],'_72.RData',sep=''))  
+    load(paste('landsat_c6_2012_eos_',scene[i-6],'.RData',sep=''))  
   }
   lphe[[i]] <- lanbymod
   mphe[[i]] <- mmphe
@@ -130,64 +130,28 @@ for(i in 1:11){
 
 ## 1:1 plot
 # all points
-for(i in 1:11){
-  if(i==1){
-    plot(mphe[[i]],pixmean[[i]],pch=i,
-         xlim=c(220,320),ylim=c(220,320),
-         main=scene[i])  
-    points(vphe[[i]],pixmean[[i]],col='red',pch=i)    
-    abline(0,1,lty=5)
-    #     abline(lm(pixmean[[i]]~mphe[[i]]))
-  }else{
-    plot(mphe[[i]],pixmean[[i]],pch=i,
-         xlim=c(220,320),ylim=c(220,320),
-         main=scene[i])    
-    points(vphe[[i]],pixmean[[i]],col='red',pch=i)    
-    abline(0,1)
-    #     abline(lm(pixmean[[i]]~mphe[[i]]))
-  } 
-}
+# for(i in 1:11){
+#   if(i==1){
+#     plot(mphe[[i]],pixmean[[i]],pch=i,
+#          xlim=c(220,320),ylim=c(220,320),
+#          main=scene[i])  
+#     points(vphe[[i]],pixmean[[i]],col='red',pch=i)    
+#     abline(0,1,lty=5)
+#     #     abline(lm(pixmean[[i]]~mphe[[i]]))
+#   }else{
+#     plot(mphe[[i]],pixmean[[i]],pch=i,
+#          xlim=c(220,320),ylim=c(220,320),
+#          main=scene[i])    
+#     points(vphe[[i]],pixmean[[i]],col='red',pch=i)    
+#     abline(0,1)
+#     #     abline(lm(pixmean[[i]]~mphe[[i]]))
+#   } 
+# }
 
 # Mean with error bars
-plot_1to1 <- function(xmean,ymean,xsd,ysd,xlabel,ylabel,start){
-  mycol <- brewer.pal(6,'Set1')
-  plot(xmean,ymean,
-       xlim=c(start,(start+60)),ylim=c(start,(start+60)),
-       xlab=xlabel,
-       ylab=ylabel,
-       cex.lab=1.1,cex.axis=1.0)
-  arrows(xmean,ymean-ysd,xmean,ymean+ysd,
-         length=0.05,angle=0)
-  arrows(xmean-xsd,ymean,xmean+xsd,ymean,
-         length=0.05,angle=0)
-  abline(0,1)
-  abline(lm(ymean~xmean),lwd=1.3,lty=5)
-  points(xmean,ymean,bg=mycol,pch=c(rep(21,6),rep(23,5)),cex=1.7)
-  legend('topleft',c('Harvard','Bartlett','Cary','Hubbard','Proctor','Boundary waters'),
-         bty='n',pch=21,pt.bg=mycol,pt.cex=1.5)
-  reg <- summary(lm(ymean~xmean))
-  rseq <- round(reg$r.squared,2)
-  b1 <- round(reg$coefficients[2],1)
-  b1c1 <- b1+round(reg$coefficients[4],1)
-  b1c2 <- b1-round(reg$coefficients[4],1)
-  b0 <- round(reg$coefficients[1],1)
-  b0c1 <- b0+round(reg$coefficients[3],1)
-  b0c2 <- b0-round(reg$coefficients[3],1)
-  rmse <- round(sqrt(mean((ymean-xmean)^2)),1)
-  text((start+33),(start+13),expression(paste(R^2,'=')),pos=4,cex=0.8)
-  text((start+38),(start+13),rseq,pos=4,cex=0.8)
-  text((start+45),(start+13),substitute(paste('(', italic(p),' < 0.01)',sep='')),pos=4,cex=0.8)
-  text((start+33),(start+9),paste('RMSE =',rmse),pos=4,cex=0.8)
-  text((start+33),(start+5),expression(paste(beta[1],'=')),pos=4,cex=0.8)
-  text((start+38),(start+5),paste(b1,' (',b1c1,',',b1c2,')',sep=''),pos=4,cex=0.8)
-  text((start+33),(start+1),expression(paste(beta[0],'=')),pos=4,cex=0.8)
-  text((start+38),(start+1),paste(b0,' (',b0c1,',',b0c2,')',sep=''),pos=4,cex=0.8)
-}
-
-par(mfrow=c(1,3),oma=c(1,1,1,1),mar=c(4,4,4,4),mgp=c(2.5,1,0))
-plot_1to1(mesd[,1],mesd[,2],mesd[,4],mesd[,5],'MODIS EOS','VIIRS EOS',220)
-plot_1to1(mesd[,1],mesd[,3],mesd[,4],mesd[,6],'MODIS EOS','Landsat EOS',220)
-plot_1to1(mesd[,2],mesd[,3],mesd[,5],mesd[,6],'VIIRS EOS','Landsat EOS',220)
+plot_1to1(mesd[,1],mesd[,2],mesd[,4],mesd[,5],'MODIS EOS','VIIRS EOS',240)
+plot_1to1(mesd[,1],mesd[,3],mesd[,4],mesd[,6],'MODIS EOS','Landsat EOS',240)
+plot_1to1(mesd[,2],mesd[,3],mesd[,5],mesd[,6],'VIIRS EOS','Landsat EOS',240)
 
 
 
