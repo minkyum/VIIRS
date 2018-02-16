499590956495f99e677cd35992ee9b2e916490dd
minkyum <moon.minkyu@gmail.com>
2018-02-16 09:26:19 -0500

    Plot codes change to function

--- 06_landsat_plot.R
+++ 06_landsat_plot.R
@@ -55,105 +55,47 @@ for(i in 1:11){
   mesd[i,6] <- sd(lphe[[i]],na.rm=T)
 }
 
-par(mfrow=c(1,3),oma=c(1,1,1,1),mar=c(4,4,4,4),mgp=c(2.5,1,0))
-mycol <- brewer.pal(6,'Set1')
-# MODIS vs VIIRS
-plot(mesd[,1],mesd[,2],
-     xlim=c(110,170),ylim=c(110,170),
-     xlab='MODIS SOS',
-     ylab='VIIRS SOS',
-     cex.lab=1.1,cex.axis=1.0)
-arrows(mesd[,1],mesd[,2]-mesd[,5],mesd[,1],mesd[,2]+mesd[,5],
-       length=0.05,angle=0)
-arrows(mesd[,1]-mesd[,4],mesd[,2],mesd[,1]+mesd[,4],mesd[,2],
-       length=0.05,angle=0)
-abline(0,1)
-abline(lm(mesd[,2]~mesd[,1]),lwd=1.3,lty=5)
-points(mesd[,1],mesd[,2],bg=mycol,pch=c(rep(21,6),rep(23,5)),cex=1.7)
-legend('topleft',c('Harvard','Bartlett','Cary','Hubbard','Proctor','Boundary waters'),
-       bty='n',pch=21,pt.bg=mycol,pt.cex=1.5)
-reg <- summary(lm(mesd[,2]~mesd[,1]))
-rseq <- round(reg$r.squared,2)
-b1 <- round(reg$coefficients[2],1)
-b1c1 <- b1+round(reg$coefficients[4],1)
-b1c2 <- b1-round(reg$coefficients[4],1)
-b0 <- round(reg$coefficients[1],1)
-b0c1 <- b0+round(reg$coefficients[3],1)
-b0c2 <- b0-round(reg$coefficients[3],1)
-rmse <- round(sqrt(mean((mesd[,2]-mesd[,1])^2)),1)
-text(143,123,expression(paste(R^2,'=')),pos=4,cex=0.8)
-text(148,123,rseq,pos=4,cex=0.8)
-text(155,123,substitute(paste('(', italic(p),' < 0.01)',sep='')),pos=4,cex=0.8)
-text(143,119,paste('RMSE =',rmse),pos=4,cex=0.8)
-text(143,115,expression(paste(beta[1],'=')),pos=4,cex=0.8)
-text(148,115,paste(b1,' (',b1c1,',',b1c2,')',sep=''),pos=4,cex=0.8)
-text(143,111,expression(paste(beta[0],'=')),pos=4,cex=0.8)
-text(148,111,paste(b0,' (',b0c1,',',b0c2,')',sep=''),pos=4,cex=0.8)
-# MODIS vs Landsat
-plot(mesd[,1],mesd[,3],
-     xlim=c(110,170),ylim=c(110,170),
-     xlab='MODIS SOS',
-     ylab='Landsat SOS',
-     cex.lab=1.1,cex.axis=1.0)
-arrows(mesd[,1],mesd[,3]-mesd[,6],mesd[,1],mesd[,3]+mesd[,6],
-       length=0.05,angle=0)
-arrows(mesd[,1]-mesd[,4],mesd[,3],mesd[,1]+mesd[,4],mesd[,3],
-       length=0.05,angle=0)
-abline(0,1)
-abline(lm(mesd[,3]~mesd[,1]),lwd=1.3,lty=5)
-points(mesd[,1],mesd[,3],bg=mycol,pch=c(rep(21,6),rep(23,5)),cex=1.7)
-# legend('topleft',c('Harvard','Bartlett','Cary','Hubbard','Proctor','Boundary waters'),
-#        bty='n',pch=21,pt.bg=mycol,cex=1.2)
-reg <- summary(lm(mesd[,3]~mesd[,1]))
-rseq <- round(reg$r.squared,2)
-b1 <- round(reg$coefficients[2],1)
-b1c1 <- b1+round(reg$coefficients[4],1)
-b1c2 <- b1-round(reg$coefficients[4],1)
-b0 <- round(reg$coefficients[1],1)
-b0c1 <- b0+round(reg$coefficients[3],1)
-b0c2 <- b0-round(reg$coefficients[3],1)
-rmse <- round(sqrt(mean((mesd[,3]-mesd[,1])^2)),1)
-text(143,123,expression(paste(R^2,'=')),pos=4,cex=0.8)
-text(148,123,rseq,pos=4,cex=0.8)
-text(155,123,substitute(paste('(', italic(p),' < 0.01)',sep='')),pos=4,cex=0.8)
-text(143,119,paste('RMSE =',rmse),pos=4,cex=0.8)
-text(143,115,expression(paste(beta[1],'=')),pos=4,cex=0.8)
-text(148,115,paste(b1,' (',b1c1,',',b1c2,')',sep=''),pos=4,cex=0.8)
-text(143,111,expression(paste(beta[0],'=')),pos=4,cex=0.8)
-text(148,111,paste(b0,' (',b0c1,',',b0c2,')',sep=''),pos=4,cex=0.8)
-# VIIRS vs Landsat
-plot(mesd[,2],mesd[,3],
-     xlim=c(110,170),ylim=c(110,170),
-     xlab='VIIRS SOS',
-     ylab='Landsat SOS',
-     cex.lab=1.1,cex.axis=1.0)
-arrows(mesd[,2],mesd[,3]-mesd[,6],mesd[,2],mesd[,3]+mesd[,6],
-       length=0.05,angle=0)
-arrows(mesd[,2]-mesd[,5],mesd[,3],mesd[,2]+mesd[,5],mesd[,3],
-       length=0.05,angle=0)
-abline(0,1)
-abline(lm(mesd[,3]~mesd[,2]),lwd=1.3,lty=5)
-points(mesd[,2],mesd[,3],bg=mycol,pch=c(rep(21,6),rep(23,5)),cex=1.7)
-# legend('topleft',c('Harvard','Bartlett','Cary','Hubbard','Proctor','Boundary waters'),
-#        bty='n',pch=21,pt.bg=mycol,cex=1.2)
-reg <- summary(lm(mesd[,3]~mesd[,2]))
-rseq <- round(reg$r.squared,2)
-b1 <- round(reg$coefficients[2],1)
-b1c1 <- b1+round(reg$coefficients[4],1)
-b1c2 <- b1-round(reg$coefficients[4],1)
-b0 <- round(reg$coefficients[1],1)
-b0c1 <- b0+round(reg$coefficients[3],1)
-b0c2 <- b0-round(reg$coefficients[3],1)
-rmse <- round(sqrt(mean((mesd[,3]-mesd[,2])^2)),1)
-text(143,123,expression(paste(R^2,'=')),pos=4,cex=0.8)
-text(148,123,rseq,pos=4,cex=0.8)
-text(155,123,substitute(paste('(', italic(p),' < 0.01)',sep='')),pos=4,cex=0.8)
-text(143,119,paste('RMSE =',rmse),pos=4,cex=0.8)
-text(143,115,expression(paste(beta[1],'=')),pos=4,cex=0.8)
-text(148,115,paste(b1,' (',b1c1,',',b1c2,')',sep=''),pos=4,cex=0.8)
-text(143,111,expression(paste(beta[0],'=')),pos=4,cex=0.8)
-text(148,111,paste(b0,' (',b0c1,',',b0c2,')',sep=''),pos=4,cex=0.8)
 
+# MODIS vs VIIRS
+plot_1to1 <- function(xmean,ymean,xsd,ysd,xlabel,ylabel){
+  mycol <- brewer.pal(6,'Set1')
+  plot(xmean,ymean,
+       xlim=c(110,170),ylim=c(110,170),
+       xlab=xlabel,
+       ylab=ylabel,
+       cex.lab=1.1,cex.axis=1.0)
+  arrows(xmean,ymean-ysd,xmean,ymean+ysd,
+         length=0.05,angle=0)
+  arrows(xmean-xsd,ymean,xmean+xsd,ymean,
+         length=0.05,angle=0)
+  abline(0,1)
+  abline(lm(ymean~xmean),lwd=1.3,lty=5)
+  points(xmean,ymean,bg=mycol,pch=c(rep(21,6),rep(23,5)),cex=1.7)
+  legend('topleft',c('Harvard','Bartlett','Cary','Hubbard','Proctor','Boundary waters'),
+         bty='n',pch=21,pt.bg=mycol,pt.cex=1.5)
+  reg <- summary(lm(ymean~xmean))
+  rseq <- round(reg$r.squared,2)
+  b1 <- round(reg$coefficients[2],1)
+  b1c1 <- b1+round(reg$coefficients[4],1)
+  b1c2 <- b1-round(reg$coefficients[4],1)
+  b0 <- round(reg$coefficients[1],1)
+  b0c1 <- b0+round(reg$coefficients[3],1)
+  b0c2 <- b0-round(reg$coefficients[3],1)
+  rmse <- round(sqrt(mean((ymean-xmean)^2)),1)
+  text(143,123,expression(paste(R^2,'=')),pos=4,cex=0.8)
+  text(148,123,rseq,pos=4,cex=0.8)
+  text(155,123,substitute(paste('(', italic(p),' < 0.01)',sep='')),pos=4,cex=0.8)
+  text(143,119,paste('RMSE =',rmse),pos=4,cex=0.8)
+  text(143,115,expression(paste(beta[1],'=')),pos=4,cex=0.8)
+  text(148,115,paste(b1,' (',b1c1,',',b1c2,')',sep=''),pos=4,cex=0.8)
+  text(143,111,expression(paste(beta[0],'=')),pos=4,cex=0.8)
+  text(148,111,paste(b0,' (',b0c1,',',b0c2,')',sep=''),pos=4,cex=0.8)
+}
+
+par(mfrow=c(1,3),oma=c(1,1,1,1),mar=c(4,4,4,4),mgp=c(2.5,1,0))
+plot_1to1(mesd[,1],mesd[,2],mesd[,4],mesd[,5],'MODIS SOS','VIIRS SOS')
+plot_1to1(mesd[,1],mesd[,3],mesd[,4],mesd[,6],'MODIS SOS','Landsat SOS')
+plot_1to1(mesd[,2],mesd[,3],mesd[,5],mesd[,6],'VIIRS SOS','Landsat SOS')
 
 
 
