dacf12e6f28a888621aa061416349871de3f7215
minkyum <moon.minkyu@gmail.com>
2018-02-27 22:05:09 -0500

    Get MODIS/VIIRS data near phenocam sites

--- 07_phenocam_data.R
+++ 07_phenocam_data.R
@@ -1,3 +1,5 @@
+## First run 01 code
+
 # Site location
 setwd('/projectnb/modislc/users/mkmoon/NEphenology/phenocam/')
 sites <- read.csv('site_modistile.csv')
@@ -10,10 +12,12 @@ path <- '/projectnb/modislc/users/mkmoon/NEphenology/phenocam/data/'
 search_str <- paste('*DB*transi*.csv',sep='')
 files <- list.files(path=path,pattern=glob2rx(search_str),full.names=F,include.dirs=F)
 
+## Get only for DB ROI files
 site.name <- sub('_DB_.*','',files)
 
+## Match site name and coordinate
 sitecord <- matrix(NA,length(site.name),4)
-rownames(sitecord) <- site.name
+# rownames(sitecord) <- site.name
 cor <- as.matrix(sites[,3:6])
 for(i in 1:length(site.name)){
   for(j in 1:nrow(cor)){
@@ -21,6 +25,7 @@ for(i in 1:length(site.name)){
   }
 }
 
+## Extract 50% SOS and EOS dates
 datesos <- matrix(NA,length(site.name),20)
 dateeos <- matrix(NA,length(site.name),20)
 colnames(datesos) <- 2001:2020
@@ -36,14 +41,87 @@ for(ss in 1:length(site.name)){
   for(i in 1:length(sos)){
     yy <- as.numeric(substr(sos[i],1,4))
     cc <- yy-2000
-    datesos[ss,cc] <- (sos[i])
+    datesos[ss,cc] <- as.Date(sos[i])
   }
   
   eos <- dat2$transition_50
   for(i in 1:length(eos)){
     yy <- as.numeric(substr(eos[i],1,4))
     cc <- yy-2000
-    dateeos[ss,cc] <- (eos[i])
+    dateeos[ss,cc] <- as.Date(eos[i])
   }
 }
+
+## Merge coordinate and dates
+phenosos <- cbind(sitecord,datesos)
+phenoeos <- cbind(sitecord,dateeos)
+
+
+### MODIS data near Phenocam sites
+get_lsp_sos <- function(sensor){
+  phe <- matrix(NA,length(site.name),25)
+  
+  for(i in 1:length(site.name)){
+    
+    if(sensor=='mm'){
+      if(phenosos[i,1]==12){
+        rast <- (mm120413[[1]]+mm120413[[2]])/2
+      }else if(phenosos[i,1]==11){
+        rast <- (mm1104[[1]]+mm1104[[2]])/2
+      }else{
+        rast <- (mm0805[[1]]+mm0805[[2]])/2
+      }
+    }else{
+      if(phenosos[i,1]==12){
+        rast <- (vv120413[[1]][[1]]+vv120413[[2]][[1]])/2
+      }else if(phenosos[i,1]==11){
+        rast <- (vv1104[[1]][[1]]+vv1104[[2]][[1]])/2
+      }else{
+        rast <- (vv0805[[1]][[1]]+vv0805[[2]][[1]])/2
+      }
+    }
+      
+    phe[i,1] <- rast[round(phenosos[i,3])-2,round(phenosos[i,4])-2]
+    phe[i,2] <- rast[round(phenosos[i,3])-2,round(phenosos[i,4])-1]
+    phe[i,3] <- rast[round(phenosos[i,3])-2,round(phenosos[i,4])]
+    phe[i,4] <- rast[round(phenosos[i,3])-2,round(phenosos[i,4])+1]
+    phe[i,5] <- rast[round(phenosos[i,3])-2,round(phenosos[i,4])+2]
+    phe[i,6] <- rast[round(phenosos[i,3])-1,round(phenosos[i,4])-2]
+    phe[i,7] <- rast[round(phenosos[i,3])-1,round(phenosos[i,4])-1]
+    phe[i,8] <- rast[round(phenosos[i,3])-1,round(phenosos[i,4])]
+    phe[i,9] <- rast[round(phenosos[i,3])-1,round(phenosos[i,4])+1]
+    phe[i,10] <- rast[round(phenosos[i,3])-1,round(phenosos[i,4])+2]
+    phe[i,11] <- rast[round(phenosos[i,3])+0,round(phenosos[i,4])-2]
+    phe[i,12] <- rast[round(phenosos[i,3])+0,round(phenosos[i,4])-1]
+    phe[i,13] <- rast[round(phenosos[i,3])+0,round(phenosos[i,4])]
+    phe[i,14] <- rast[round(phenosos[i,3])+0,round(phenosos[i,4])+1]
+    phe[i,15] <- rast[round(phenosos[i,3])+0,round(phenosos[i,4])+2]
+    phe[i,16] <- rast[round(phenosos[i,3])+1,round(phenosos[i,4])-2]
+    phe[i,17] <- rast[round(phenosos[i,3])+1,round(phenosos[i,4])-1]
+    phe[i,18] <- rast[round(phenosos[i,3])+1,round(phenosos[i,4])]
+    phe[i,19] <- rast[round(phenosos[i,3])+1,round(phenosos[i,4])+1]
+    phe[i,20] <- rast[round(phenosos[i,3])+1,round(phenosos[i,4])+2]
+    phe[i,21] <- rast[round(phenosos[i,3])+2,round(phenosos[i,4])-2]
+    phe[i,22] <- rast[round(phenosos[i,3])+2,round(phenosos[i,4])-1]
+    phe[i,23] <- rast[round(phenosos[i,3])+2,round(phenosos[i,4])]
+    phe[i,24] <- rast[round(phenosos[i,3])+2,round(phenosos[i,4])+1]
+    phe[i,25] <- rast[round(phenosos[i,3])+2,round(phenosos[i,4])+2]
+  }
+  
+  return(phe)
+}
+
+phevv <- get_lsp_sos('vv')
+phemm <- get_lsp_sos('mm')
+
+
+sosvv <- apply(phevv,1,mean)
+sosmm <- apply(phemm,1,mean)
+sospc <- as.Date(phenosos[,17],origin='1970-1-1')
+sospc <- as.numeric(strftime(sospc,format='%j'))
+
+plot(sosvv,sospc,xlim=c(90,170),ylim=c(90,170),pch=19,col='red')
+points(sosmm,sospc,pch=19,col='blue')
+abline(0,1)
+
 
